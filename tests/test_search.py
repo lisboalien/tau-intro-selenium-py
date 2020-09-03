6e01963ef2e0415a8cc64e18a1bd9880fcd5836b
Aline de Farias Lisboa <lisboalien@gmail.com>
2020-09-02 22:01:21 -0300

    Complete course Selenium Webdriver with Python from TAU

---
+++ tests/test_search.py
@@ -0,0 +1,31 @@
+"""
+These tests cover DuckDuckGo searches
+"""
+
+import pytest
+
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+    PHRASE = "panda"
+    
+    # Given the DuckDuckGo home page is displayed
+    search_page.load()
+
+    # When the user searches for "panda"
+    search_page.search(PHRASE)
+
+    # Then the search result query is "panda"
+    assert PHRASE == result_page.search_input_value()
+
+    # And the search result links pertain to "panda"
+    titles = result_page.result_link_titles()
+    matches = [t for t in titles if PHRASE.lower() in t.lower()]
+    assert len(matches) > 0
+
+    # And the search result title contains "panda"
+    assert PHRASE in result_page.title()
